83023bac0340d01f7aa42d504e28a62b186fe023
donguk <ldu2175@gmail.com>
2020-03-17 17:13:49 +0900

    swea 6782 현주가 좋아하는 제곱근 놀이

---
+++ Algorithm/swea 6782 현주가 좋아하는 제곱근 놀이/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int solve(long long n) {
+	int cnt = 0;
+	while (n != 2) {
+		// 제곱근이 정수라면 제곱근을 취한다.
+		if ((int)sqrt(n) == sqrt(n)) {
+			n = sqrt(n);
+			cnt += 1;
+		}
+		// 제곱근이 정수가 아니라면 다음 제곱 수 까지 1을 더하다가 만나면 제곱근을 취한다.
+		else {
+			long long next_squ = (long long)(sqrt(n) + 1);
+			cnt += (next_squ * next_squ) - n + 1;
+			n = next_squ;
+		}
+	}
+	return cnt;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin >> t;
+	for (int tc = 0; tc < t; tc++) {
+		long long n;
+		cin >> n;
+		cout << "#" << tc + 1 << " " << solve(n) << "\n";
+	}
+
+	return 0;
+}
