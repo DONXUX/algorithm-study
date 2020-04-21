adf99a920f4ca8dfe7c8ce064c711621116b4355
donguk <ldu2175@gmail.com>
2020-04-22 02:35:12 +0900

    2512 예산

--- Algorithm/2512 예산/main.cpp
+++ Algorithm/2512 예산/main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
  
-int n, country;
+int n, m;
 int money[10000];
 
 int solve() {
-    
+    int lo = 0, hi = m;
+
+    int money_max = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += money[i];
+        money_max = max(money_max, money[i]);
+    }
+    if (sum <= m) return money_max;
+
+    while (lo + 1 < hi) {
+        int mid = (lo + hi) / 2;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (money[i] > mid) sum += mid;
+            else sum += money[i];
+        }
+        if (sum <= m) lo = mid;
+        else hi = mid;
+    }
+
+    return lo;
 }
 
 void input() {
     cin >> n;
     for (int i = 0; i < n; i++)
         cin >> money[i];
-    cin >> country;
+    cin >> m;
 }
 
 int main(void) {
